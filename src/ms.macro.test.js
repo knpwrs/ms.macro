b5a46ce283f1755173681c6527efb08cde45f4b5
Kenneth Powers <ken@kenpowers.net>
2017-09-26 22:51:45 -0400

    Add test for using ms and ms.macro together.

--- src/ms.macro.test.js
+++ src/ms.macro.test.js
@@ -9,30 +9,32 @@ const run = code => transform(code, {
   filename: __filename,
 }).code.trim();
 
-const expected = stripIndent`
-  const ONE_DAY = 86400000;
-  const TWO_DAYS = 172800000;
-`;
-
-test('CallExpression', (t) => {
-  const input = stripIndent`
-    import ms from './ms.macro';
-    const ONE_DAY = ms('1 day');
-    const TWO_DAYS = ms('2 days');
+{
+  const expected = stripIndent`
+    const ONE_DAY = 86400000;
+    const TWO_DAYS = 172800000;
   `;
-  const output = run(input);
-  t.is(output, expected);
-});
 
-test('TaggedTemplateExpression', (t) => {
-  const input = stripIndent`
-    import ms from './ms.macro';
-    const ONE_DAY = ms\`1 day\`;
-    const TWO_DAYS = ms\`2 days\`;
-  `;
-  const output = run(input);
-  t.is(output, expected);
-});
+  test('CallExpression', (t) => {
+    const input = stripIndent`
+      import ms from './ms.macro';
+      const ONE_DAY = ms('1 day');
+      const TWO_DAYS = ms('2 days');
+    `;
+    const output = run(input);
+    t.is(output, expected);
+  });
+
+  test('TaggedTemplateExpression', (t) => {
+    const input = stripIndent`
+      import ms from './ms.macro';
+      const ONE_DAY = ms\`1 day\`;
+      const TWO_DAYS = ms\`2 days\`;
+    `;
+    const output = run(input);
+    t.is(output, expected);
+  });
+}
 
 test('CallExpression Error', (t) => {
   const input = stripIndent`
@@ -41,4 +43,21 @@ test('CallExpression Error', (t) => {
   `;
   const error = t.throws(() => run(input));
   t.is(error.message, `${__filename}: Invalid input given to ms.macro at line 2`);
+});
+
+test('Use ms and ms.macro together', (t) => {
+  const input = stripIndent`
+    import ms from 'ms';
+    import msm from './ms.macro';
+    const ONE_DAY = msm('1 day');
+    const str = ms(172800000);
+  `;
+  const expected = stripIndent`
+    import ms from 'ms';
+
+    const ONE_DAY = 86400000;
+    const str = ms(172800000);
+  `;
+  const output = run(input);
+  t.is(output, expected);
 });
