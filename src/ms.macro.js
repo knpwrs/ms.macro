a17c9549039b72ed545fb4791a79b9271da9a332
Kenneth Powers <ken@kenpowers.net>
2017-09-26 21:45:04 -0400

    Add TaggedTemplateLiteral functionality.

--- src/ms.macro.js
+++ src/ms.macro.js
@@ -4,7 +4,11 @@ import ms from 'ms';
 module.exports = createMacro(({ babel: { types: t }, references: { default: paths } }) => {
   paths.forEach(({ parentPath }) => {
     if (parentPath.type === 'CallExpression') {
-      parentPath.replaceWith(t.numericLiteral(ms(parentPath.node.arguments[0].value)));
+      const value = ms(parentPath.node.arguments[0].value);
+      parentPath.replaceWith(t.numericLiteral(value));
+    } else if (parentPath.type === 'TaggedTemplateExpression') {
+      const value = ms(parentPath.node.quasi.quasis[0].value.cooked);
+      parentPath.replaceWith(t.numericLiteral(value));
     }
   });
 });
--- src/ms.macro.test.js
+++ src/ms.macro.test.js
@@ -3,20 +3,33 @@ import macroPlugin from 'babel-macros';
 import { transform } from 'babel-core';
 import { stripIndent } from 'common-tags';
 
+const run = code => transform(code, {
+  babelrc: false,
+  plugins: [macroPlugin],
+  filename: __filename,
+}).code.trim();
+
+const expected = stripIndent`
+  const ONE_DAY = 86400000;
+  const TWO_DAYS = 172800000;
+`;
+
 test('CallExpression', (t) => {
   const input = stripIndent`
     import ms from './ms.macro';
     const ONE_DAY = ms('1 day');
     const TWO_DAYS = ms('2 days');
   `;
-  const expected = stripIndent`
-    const ONE_DAY = 86400000;
-    const TWO_DAYS = 172800000;
+  const output = run(input);
+  t.is(output, expected);
+});
+
+test('TaggedTemplateExpression', (t) => {
+  const input = stripIndent`
+    import ms from './ms.macro';
+    const ONE_DAY = ms\`1 day\`;
+    const TWO_DAYS = ms\`2 days\`;
   `;
-  const output = transform(input, {
-    babelrc: false,
-    plugins: [macroPlugin],
-    filename: __filename,
-  }).code.trim();
+  const output = run(input);
   t.is(output, expected);
 });
